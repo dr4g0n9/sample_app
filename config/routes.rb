96579304caea1db7a5003de0c70df8a255ddbbcb
Alan John Morton <Alan.john.morton@gmail.com>
2013-03-28 00:05:50 -0400

    /

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ SampleApp::Application.routes.draw do
 
   get "pages/contact"
 
-  get "pages/app/assets/stylesheets/"
-  # The priority is based upon order of creation:
+    # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
