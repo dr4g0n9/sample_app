ce713c49c72a0478683cb3b55039019efdd2dd62
Alan John Morton <Alan.john.morton@gmail.com>
2013-04-05 15:52:34 -0400

    jquery change

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,26 +1,53 @@
 var minutes = 0;
 var seconds = 0;
+var hours = 0;
 var int = 0;
 
-function timer() 
-{
-	document.getElementById("clock").innerHTML = minutes + ":" + seconds;
-	document.getElementById("timer").innerHTML = "<button id=\"timerButton\" type=\"button\" class=\"round\" onclick=\"abort()\"> Stop Timer </button>";
+function timer(){
+
+	if(seconds < 10 && minutes < 10)
+		$("#clock").html("0" + minutes + ":0" + seconds);
+
+	if(seconds < 10 && minutes >= 10)
+		$("#clock").html(minutes + ":0" + seconds);
+
+	if(seconds >= 10 && minutes < 10)
+		$("#clock").html("0" + minutes + ":" + seconds);
+	
 	if(seconds == 59)
 	{
 		seconds = 0;
 		if(minutes == 59)
 		{
 			minutes = 0;
+			if(hours == 99)
+			{
+				hours = 0;
+			}else hours++;
 		} else minutes++;
 	}else seconds++;
 
-	startTimer();
+	start();
 }
 
-function startTimer(){ int = setTimeout(timer, 1000) };
+function reset(){
+	abort();
+	minutes = 0;
+	seconds = 0;
+	hours = 0;
+	$("#timerButton").html("Start Timer");
+	$("#timerButton").attr("onclick", "start()");
+	$("#clock").html("00:00");
+}
+
+function start(){ 
+	int = setTimeout(timer, 1000);
+	$("#timerButton").html("Stop Timer");
+	$("#timerButton").attr("onclick", "abort()");
+}
 
 function abort(){
 	clearInterval(int);
-	document.getElementById("timer").innerHTML = "<button id=\"timerButton\" type=\"button\" class=\"round\" onclick=\"startTimer()\"> Start Timer </button>";
+	$("#timerButton").html("Start Timer");
+	$("#timerButton").attr("onclick", "start()");
 }
